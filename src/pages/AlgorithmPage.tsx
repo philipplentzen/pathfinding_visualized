f3064611db3b41b59dc9cbc005233461889e26c7
Philipp Lentzen <philen1411@gmail.com>
2021-01-19 20:24:38 +0100

    Adds algorithm page

---
+++ src/pages/AlgorithmPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface IAlgorithmPageProps {
+
+}
+
+export const AlgorithmPage: React.FC<IAlgorithmPageProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
