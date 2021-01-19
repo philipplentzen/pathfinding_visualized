10374a0407c1168cbc23a0601fd0926d6524f090
Philipp Lentzen <philen1411@gmail.com>
2021-01-19 23:38:53 +0100

    Adds shadow to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import {PageHeader, Tag} from "antd";
+import styled from "styled-components";
 
 interface IHeaderProps {
 
 }
 
+const HeaderContainer = styled.header`
+    box-shadow: 0 6px 16px -8px rgba(0,0,0,.08), 0 9px 28px 0 rgba(0,0,0,.05), 0 12px 48px 16px rgba(0,0,0,.03);
+`
+
 export const Header: React.FC<IHeaderProps> = () => {
     return (
-        <PageHeader title="Pathfinding Visualized"
-                    tags={<Tag color="cyan">ALPHA</Tag>}
-                    ghost={false}/>
+        <HeaderContainer>
+            <PageHeader title="Pathfinding Visualized"
+                        tags={<Tag color="cyan">ALPHA</Tag>}
+                        ghost={false} />
+        </HeaderContainer>
     );
 }
