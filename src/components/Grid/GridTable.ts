c935a41ef7516a1654e190c57e0c5879abc4dc94
Philipp Lentzen <philen1411@gmail.com>
2021-01-24 01:22:21 +0100

    Updates css

--- src/components/Grid/GridTable.ts
+++ src/components/Grid/GridTable.ts
@@ -4,6 +4,6 @@ export const GridTable = styled.div`
     display: table;
     border-collapse: collapse;
     margin: auto;
-    width: 100%;
-    height: 100%;
+    width: fit-content;
+    height: fit-content;
 `
