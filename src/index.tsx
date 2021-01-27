5ffed6bb503ba6005479ccf4bc6764745dff1ed2
Philipp Lentzen <philen1411@gmail.com>
2021-01-28 00:55:42 +0100

    Adds ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import "antd/dist/antd.css";
 import {Router} from "./Router";
 import {GlobalStyle} from "./styles/GlobalStyle";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,3 +18,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
